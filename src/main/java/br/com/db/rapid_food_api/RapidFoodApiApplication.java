48f8f8f279cf264d5e6d6fc8b896a811c328213c
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-06-25 10:57:18 -0300

    chore: add initial project setup

---
+++ src/main/java/br/com/db/rapid_food_api/RapidFoodApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.db.rapid_food_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidFoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapidFoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/db/rapid_food_api/RapidFoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.db.rapid_food_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapidFoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
