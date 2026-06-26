b6dc2c8cfb4317ac22b73814b449a2ccc24c4e7a
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-06-26 13:06:32 -0300

    test: ensure that the user can be retrieved in user service

--- src/test/java/br/com/db/rapid_food_api/user/service/UserServiceTest.java
+++ src/test/java/br/com/db/rapid_food_api/user/service/UserServiceTest.java
@@ -1,5 +1,7 @@
 package br.com.db.rapid_food_api.user.service;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.Test;
@@ -15,59 +17,91 @@ import static br.com.db.rapid_food_api.user.common.UserConstants.CREATE_USER_REQ
 import static br.com.db.rapid_food_api.user.common.UserConstants.EMAIL;
 import static br.com.db.rapid_food_api.user.common.UserConstants.PASSWORD;
 import static br.com.db.rapid_food_api.user.common.UserConstants.PASSWORD_HASH;
+import static br.com.db.rapid_food_api.user.common.UserConstants.USER_ID;
 import static br.com.db.rapid_food_api.user.common.UserConstants.createUser;
 import static br.com.db.rapid_food_api.user.common.UserConstants.createUserResponse;
 import br.com.db.rapid_food_api.user.domain.User;
 import br.com.db.rapid_food_api.user.dto.UserResponse;
 import br.com.db.rapid_food_api.user.exception.EmailAlreadyExistsException;
+import br.com.db.rapid_food_api.user.exception.UserNotFoundException;
 import br.com.db.rapid_food_api.user.mapper.UserMapper;
 import br.com.db.rapid_food_api.user.repository.UserRepository;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
-    @InjectMocks
-    private UserService userService;
+        @InjectMocks
+        private UserService userService;
 
-    @Mock
-    private UserRepository userRepository;
+        @Mock
+        private UserRepository userRepository;
 
-    @Mock
-    private UserMapper userMapper;
+        @Mock
+        private UserMapper userMapper;
 
-    @Mock
-    private PasswordEncoder passwordEncoder;
+        @Mock
+        private PasswordEncoder passwordEncoder;
 
-    @Test
-    void shouldCreateUserWithValidData() {
-        User user = createUser();
-        UserResponse expectedResponse = createUserResponse();
-        when(userRepository.existsByEmail(EMAIL))
-                .thenReturn(false);
-        when(passwordEncoder.encode(PASSWORD))
-                .thenReturn(PASSWORD_HASH);
-        when(userMapper.toEntity(CREATE_USER_REQUEST, PASSWORD_HASH))
-                .thenReturn(user);
-        when(userRepository.save(any(User.class)))
-                .thenReturn(user);
-        when(userMapper.toResponse(user))
-                .thenReturn(expectedResponse);
+        @Test
+        void shouldCreateUserWithValidData() {
+                User user = createUser();
+                UserResponse expectedResponse = createUserResponse();
+                when(userRepository.existsByEmail(EMAIL))
+                                .thenReturn(false);
+                when(passwordEncoder.encode(PASSWORD))
+                                .thenReturn(PASSWORD_HASH);
+                when(userMapper.toEntity(CREATE_USER_REQUEST, PASSWORD_HASH))
+                                .thenReturn(user);
+                when(userRepository.save(any(User.class)))
+                                .thenReturn(user);
+                when(userMapper.toResponse(user))
+                                .thenReturn(expectedResponse);
 
-        UserResponse response = userService.create(CREATE_USER_REQUEST);
+                UserResponse response = userService.create(CREATE_USER_REQUEST);
 
-        assertThat(response).isNotNull();
-        assertThat(response.name()).isEqualTo(CREATE_USER_REQUEST.name());
-        assertThat(response.email()).isEqualTo(CREATE_USER_REQUEST.email());
-        assertThat(response.active()).isTrue();
-    }
+                assertThat(response).isNotNull();
+                assertThat(response.name()).isEqualTo(CREATE_USER_REQUEST.name());
+                assertThat(response.email()).isEqualTo(CREATE_USER_REQUEST.email());
+                assertThat(response.active()).isTrue();
+        }
 
-    @Test
-    void shouldThrowEmailAlreadyExistsExceptionWhenEmailAlreadyExists() {
-        when(userRepository.existsByEmail(EMAIL))
-                .thenReturn(true);
+        @Test
+        void shouldThrowEmailAlreadyExistsExceptionWhenEmailAlreadyExists() {
+                when(userRepository.existsByEmail(EMAIL))
+                                .thenReturn(true);
 
-        assertThatThrownBy(() -> userService.create(CREATE_USER_REQUEST))
-                .isInstanceOf(EmailAlreadyExistsException.class)
-                .hasMessage("Já existe um usuário cadastrado com o e-mail: miguel@email.com");
-    }
+                assertThatThrownBy(() -> userService.create(CREATE_USER_REQUEST))
+                                .isInstanceOf(EmailAlreadyExistsException.class)
+                                .hasMessage("Já existe um usuário cadastrado com o e-mail: miguel@email.com");
+        }
+
+        @Test
+        void shouldGetUserByIdWhenUserExists() {
+                User user = createUser();
+                UserResponse expectedResponse = createUserResponse();
+
+                when(userRepository.findById(USER_ID))
+                                .thenReturn(Optional.of(user));
+
+                when(userMapper.toResponse(user))
+                                .thenReturn(expectedResponse);
+
+                UserResponse response = userService.getById(USER_ID);
+
+                assertThat(response).isNotNull();
+                assertThat(response.id()).isEqualTo(expectedResponse.id());
+                assertThat(response.name()).isEqualTo(expectedResponse.name());
+                assertThat(response.email()).isEqualTo(expectedResponse.email());
+                assertThat(response.active()).isTrue();
+        }
+
+        @Test
+        void shouldThrowResourceNotFoundExceptionWhenUserDoesNotExistById() {
+                when(userRepository.findById(USER_ID))
+                                .thenReturn(Optional.empty());
+
+                assertThatThrownBy(() -> userService.getById(USER_ID))
+                                .isInstanceOf(UserNotFoundException.class)
+                                .hasMessage("User not found");
+        }
 }
