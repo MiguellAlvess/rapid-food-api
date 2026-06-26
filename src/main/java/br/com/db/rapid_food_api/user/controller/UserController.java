445035efa41d0682c4fba93c6daa030503b03fec
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-06-26 13:29:40 -0300

    feat: add update user route to user controller

--- src/main/java/br/com/db/rapid_food_api/user/controller/UserController.java
+++ src/main/java/br/com/db/rapid_food_api/user/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.db.rapid_food_api.user.dto.CreateUserRequest;
+import br.com.db.rapid_food_api.user.dto.UpdateUserRequest;
 import br.com.db.rapid_food_api.user.dto.UserResponse;
 import br.com.db.rapid_food_api.user.service.UserService;
 import jakarta.validation.Valid;
@@ -33,5 +35,12 @@ public class UserController {
     @GetMapping("/{id}")
     public UserResponse getById(@PathVariable UUID id) {
         return userService.getById(id);
+    }
+
+    @PatchMapping("/{id}")
+    public UserResponse update(
+            @PathVariable UUID id,
+            @RequestBody @Valid UpdateUserRequest request) {
+        return userService.update(id, request);
     }
 }
