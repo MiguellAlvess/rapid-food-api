f747e894b44b2e8d5d1a858270449880ad17f220
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-06-26 09:28:51 -0300

    feat: add user controller with create user route

---
+++ src/main/java/br/com/db/rapid_food_api/user/controller/UserController.java
@@ -0,0 +1,28 @@
+package br.com.db.rapid_food_api.user.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.db.rapid_food_api.user.dto.CreateUserRequest;
+import br.com.db.rapid_food_api.user.dto.UserResponse;
+import br.com.db.rapid_food_api.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserResponse create(@RequestBody @Valid CreateUserRequest request) {
+        return userService.create(request);
+    }
+}
