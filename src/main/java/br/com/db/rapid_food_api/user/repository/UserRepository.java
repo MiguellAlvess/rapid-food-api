ad99ef9e137e49ce294e653411bc3a00fbf550ce
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-06-26 09:23:29 -0300

    feat: add user repository with exists by email method

---
+++ src/main/java/br/com/db/rapid_food_api/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package br.com.db.rapid_food_api.user.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.db.rapid_food_api.user.domain.User;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    boolean existsByEmail(String email);
+}
