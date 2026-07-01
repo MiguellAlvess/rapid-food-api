358cc651763673ce9b57edc6e6dd42120f638a14
danielavescia <danivescia@gmail.com>
2026-07-01 09:57:32 -0300

    refactor: remove unused Testcontainers annotations from integration test base classes

--- src/test/java/br/com/db/rapid_food_api/config/PostgresContainerConfig.java
+++ src/test/java/br/com/db/rapid_food_api/config/PostgresContainerConfig.java
@@ -2,12 +2,14 @@ package br.com.db.rapid_food_api.config;
 
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
+
 
 public class PostgresContainerConfig {
 
-    @Container
     @ServiceConnection
     static final PostgreSQLContainer<?> POSTEGRES = new PostgreSQLContainer<>("postgres:17.4");
 
+    static{
+        POSTEGRES.start();
+    }
 }
